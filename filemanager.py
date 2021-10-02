65b3086328731185c3cc62c5b5bfe545065a23ca
Joshua Tenorio <joshliam09@gmail.com>
2021-10-02 16:57:18 -0700

    update allergies is working

--- filemanager.py
+++ filemanager.py
@@ -4,9 +4,7 @@ import json
 def getAuthors():
     with open('authors.json') as file:
         data = json.load(file)
-        print(type(data['authors'])) # this is list
         userList = data['authors']
-        print(data['authors'][0]['author'])
     return userList
 
 def addAuthor(author):
@@ -16,13 +14,18 @@ def setPreferences(author, preferences):
     pass
 
 def setAllergies(author, allergies):
-    pass
+    users = getAuthors()
+    for u in users:
+        if u['author'] == author:
+            u["allergies"] = allergies
+    dictionary = {"authors": users}
+    with open('authors.json', "w") as file:
+        json.dump(dictionary, file)
 
 def getAuthor(tag):
     users = getAuthors()
     for u in users:
         if u['author'] == tag:
-            print("found u")
             return u
 
 def getPreferences(tag):
@@ -33,4 +36,5 @@ def getAllergies(tag):
     u = getAuthor(tag)
     return u["allergies"]
 
-
+setAllergies("tenmo#6399", ["boop"])
+print(getAllergies("tenmo#6399"))
