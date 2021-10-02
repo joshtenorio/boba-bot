dede6accbd808694eb2c76701fb4337b3dd19f69
JardRily <JaredTreyRiley@gmail.com>
2021-10-02 15:13:19 -0700

    Changed Tabbing

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ client = discord.Client()
 async def on_ready():
         print('We have logged in as {0.user}'.format(client))
 
-        @client.event
-        async def on_message(message):
-                print(message.content)
-                print(message.author)
-                if message.author == client.user:
-                            return
+@client.event
+async def on_message(message):
+        print(message.content)
+        print(message.author)
+        if message.author == client.user:
+                    return
 
-                            if message.content.startswith('$hello'):
-                                        await message.channel.send('Hello!')
+                    if message.content.startswith('$hello'):
+                                await message.channel.send('Hello!')
 
 client.run('ODkzOTY0NTE3OTc1OTQ1MjU2.YVjHVg.OHTxuCtXYcIkWLupRMWRavs5pAU')
