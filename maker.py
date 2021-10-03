6ecf575904f59435ceca9b38a7b880447e86994f
Joshua Tenorio <joshliam09@gmail.com>
2021-10-02 17:58:27 -0700

    restarting make function

--- maker.py
+++ maker.py
@@ -2,13 +2,7 @@ import discord
 import filemanager as fm
 
 def makeBoba(message):
-    userTag = message.author
-    authObject = fm.getAuthor(userTag)
-    if authObject == -1:
-        fm.addAuthor(userTag)
-
-        # alternatively just use getPreferences/getAllergies
-        authObject = fm.getAuthor(userTag)
-
-
-
+    u = fm.getAuthor(message.author)
+    if u == -1:
+        fm.addAuthor(message.author)
+        u = fm.getAuthor(message.author)
